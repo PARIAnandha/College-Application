a5f59927b625cef3b00918775046b7467aff4b1f
PARIAnandha <parianandha@gmail.com>
2024-11-18 04:17:02 +0530

    add

--- frontend/src/component/api.js
+++ frontend/src/component/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "https://college-application-backend.onrender.com";
+const API_BASE_URL = "Api add";
 
 export const submitApplication = async (applicationData) => {
   try {
